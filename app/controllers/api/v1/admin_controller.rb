616aea5d11f5b4873ee2d0a361199b93ae6f51b3
shan <2302531309@qq.com>
2024-09-16 11:51:40 +0800

    feat: 指定ip的可以访问后台

--- app/controllers/api/v1/admin_controller.rb
+++ app/controllers/api/v1/admin_controller.rb
@@ -3,6 +3,12 @@ class Api::V1::AdminController < ApplicationController
   before_action :check_authorization!
 
   def check_authorization!
-    authorize :admin, :admin?
+    # authorize :admin, :admin?
+
+    if current_user&.email != ENV.fetch('ADMIN_USER_EMAIL') && request.remote_ip != ENV.fetch('ADMIN_USER_IP')
+      render json: {
+        message: 'User not authorized, only admin can access'
+      }.to_json, status: 403
+    end
   end
 end
