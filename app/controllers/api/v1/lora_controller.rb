21d799d5fc6912319c463e4a3195de06611bed1a
shan <2302531309@qq.com>
2024-10-22 23:44:17 +0800

    seo: 更改页面 seo title 和 h1 等内容

--- app/controllers/api/v1/lora_controller.rb
+++ app/controllers/api/v1/lora_controller.rb
@@ -12,16 +12,18 @@ class Api::V1::LoraController < ApplicationController
                     end
 
     loras = lora_category.map do |lora|
+      display_name = lora.value.split('/').last.split('-').map(&:capitalize).join(' ')
       {
         name: I18n.t('lora.name', model_name: lora.name, cost_credits: lora.cost_credits),
+        display_name: display_name,
         value: lora.value,
         cost_credits: lora.cost_credits,
         runs: lora.runs,
-        seo_title: I18n.t('lora.title', model: lora.value),
-        seo_description: I18n.t('lora.description', model: lora.value),
-        h1: I18n.t('lora.h1', model: lora.value),
-        h1_p: I18n.t('lora.h1_p', model: lora.value),
-        lora_description: I18n.t("lora.lora_description.#{lora.value}", default: ''),
+        seo_title: I18n.t('lora.title', model: display_name),
+        seo_description: I18n.t('lora.description', model: display_name),
+        h1: I18n.t('lora.h1', model: display_name),
+        h1_p: I18n.t('lora.h1_p', model: display_name),
+        lora_description: I18n.t("lora.lora_description.#{display_name}", default: ''),
         example_pics: lora.showcases.map {|i| { id: i.id, image: (url_for(i.image) rescue nil) }}
       }
     end
